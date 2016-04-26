962fda58957b1f93287a670fac9030c0b8ca15e9
gvillalta99 <gvillalta99@gmail.com>
2016-04-26 14:39:00 -0300

    Visiting Enrollment page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,15 +4,22 @@ RSpec.describe 'Home page', type: :feature do
   describe 'visiting the students page' do
     it 'visits the students listing page' do
       visit root_path
-      click_on "Students Page!"
+      click_on 'Students Page!'
       expect(page).to have_content 'Listing students '
     end
   end
   describe 'visiting the courses page' do
     it 'visits the courses listing page' do
       visit root_path
-      click_on "Courses"
+      click_on 'Courses Page!'
       expect(page).to have_content 'Listing Courses'
+    end
+  end
+  describe 'visiting the enrollment page' do
+    it 'visits the enrollment page' do
+      visit root_path
+      click_on 'Enrollments Page!'
+      expect(page).to have_content 'New Enrollment'
     end
   end
 end
